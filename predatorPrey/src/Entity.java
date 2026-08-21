ce467fee87649cd6b8e8d65140c6fcc5812f7a95
Oscar <oscarehalliday@gmail.com>
2026-08-21 12:50:39 +1000

    Created Entity, Grass and Creature (WIP) classes

---
+++ predatorPrey/src/Entity.java
@@ -0,0 +1,11 @@
+public class Entity {
+     int x;
+     int y; 
+     boolean isFood;
+
+     public Entity(int x, int y, boolean isFood) {
+         this.x = x;
+         this.y = y;
+         this.isFood = isFood;
+     }
+}
