5a45a5486d4f59dd28665fd556c4101c6bd3dbf0
Zixuan740 <zixuan.chen3@students.mq.edu.au>
2026-08-25 09:58:16 +0800

    Fixed compilation errors

--- predatorPrey/src/Prey.java
+++ predatorPrey/src/Prey.java
@@ -1,9 +1,10 @@
 public class Prey extends Creature {
     Boolean inDanger;
 
-
-    public Prey(int x, int y, int speed) {
-        super(x, y, speed);
+    public Prey(int speed, int hunger, boolean isFood, int x, int y) {
+        super(speed, hunger, isFood, x, y);
+        this.isFood = isFood;
+        
     }
     
     public void Movement() {
