61e579f315f8ba09f77f16113f2024c248d97a27
auyondebnath-cpu <auyon.debnath@students.mq.edu.au>
2026-08-26 12:37:57 +1000

    Encapsulate Entity class fields and add getter methods

--- predatorPrey/src/Entity.java
+++ predatorPrey/src/Entity.java
@@ -1,11 +1,23 @@
 public class Entity {
-     int x;
-     int y; 
-     boolean isFood;
+     private int x;
+     private int y; 
+     private boolean isFood;
 
      public Entity(int x, int y, boolean isFood) {
          this.x = x;
          this.y = y;
          this.isFood = isFood;
+     }
+
+     public int getX(){
+        return x;
+     }
+
+     public int getY(){
+        return y;
+     }
+
+     public boolean isFood(){
+        return isFood;
      }
 }
