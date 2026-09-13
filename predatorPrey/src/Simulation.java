fa176419c67a2506cf32d75121bd6b45712ca254
Tal48272000 <tallen.britten@students.mq.edu.au>
2026-09-13 22:36:16 +1000

    FirstDisplaySteps

---
+++ predatorPrey/src/Simulation.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Simulation {
+    public static void main(String[] args) throws Exception {
+        List<Creature> sim = new ArrayList<>();
+        List<Grass> grassList = new ArrayList<>();
+
+        JFrame simStart = new JFrame("Predator, Prey Simulation");
+        simStart.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        simStart.setSize(800, 700);
+        simStart.setLocationRelativeTo(null);
+        simStart.setResizable(false);
+        simStart.setVisible(true);
+
+    }
+}
