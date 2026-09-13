f00bf7a45f6169fbe4caddf2b7b3002616d9dc50
Tal48272000 <tallen.britten@students.mq.edu.au>
2026-09-14 01:03:45 +1000

    Not quite Working button
    Still forming the basis of the start button trying to understand the necessary methods and the imports and such.

--- predatorPrey/src/Simulation.java
+++ predatorPrey/src/Simulation.java
@@ -3,22 +3,43 @@ import java.util.List;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import java.util.EventListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Simulation {
+
     public static void main(String[] args) throws Exception {
         List<Creature> sim = new ArrayList<>();
         List<Grass> grassList = new ArrayList<>();
+    }
 
-        JFrame simStart = new JFrame("Predator, Prey Simulation");
+    public JFrame simStart;
+    public JPanel panel;
+    public JButton begin;
+
+    public Simulation(){
+        frameInitialise();
+    }
+
+    private void frameInitialise(){
+        
+        simStart = new JFrame("Predator, Prey Simulation");
         simStart.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         simStart.setSize(800, 700);
         simStart.setLocationRelativeTo(null);
         simStart.setResizable(false);
 
-        JPanel panel = new JPanel();
-        JButton begin;
+        panel = new JPanel();
         begin = new JButton("Start");
         
+        begin.addActionListener(new ActionListener() {
+            @Override
+            public void startClicked(ActionEvent e){
+                System.out.println("Button"); 
+            }
+        });
+
         panel.add(begin);
         simStart.add(panel);
         simStart.setVisible(true);
