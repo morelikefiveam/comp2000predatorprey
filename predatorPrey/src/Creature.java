4bdd1f47c65e881e28d93a99a1caed4352632f12
Corbin Phillips <corbin.phillips@students.mq.edu.au>
2026-09-15 09:44:52 +1000

    Revert "Merge branch 'main' of https://github.com/morelikefiveam/comp2000predatorprey"
    This reverts commit 15185c80e9488158f9c4954e97980d556d99c67c, reversing
    changes made to 324514b261bca1e674713f703d5463c1adce01be.

--- predatorPrey/src/Creature.java
+++ predatorPrey/src/Creature.java
@@ -1,84 +1,13 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+public class Creature extends Entity {
+    private int speed;
+    private int hunger;
+    
 
-public abstract class Creature extends Entity {
-
-    private int speed; 
-    private int starvation;
-    private int lastX;
-    private int lastY;
-    private int dx = 1;
-    private int dy = 1;
-
-    private static final int STARVATION_THRESHOLD = 100;
-
-    public Creature(int speed, int starvation, boolean isFood, int x, int y) {
+    public Creature(int speed, int hunger, boolean isFood, int x, int y) {
         super(x, y, isFood);
         this.speed = speed;
-        this.starvation = starvation;
-        this.lastX = x;
-        this.lastY = y;
+        this.hunger = hunger;
     }
-
-    protected abstract int getStarvationRate();
-
-    public boolean starve(){
-        starvation += getStarvationRate();
-        return starvation >= STARVATION_THRESHOLD;
-    }
-
-    protected void recordPosition(){
-        lastX = getX();
-        lastY = getY();
-    }
-
-    public int getVelocityX(){
-        return getX() - lastX;
-    }
-
-    public int getVelocityY(){
-        return getY() - lastY;
-    }
-
-    protected void moveWithBounce(int panelWidth, int panelHeight){
-        int newX = getX() + dx * getSpeed();
-        int newY = getY() + dy * getSpeed();
-
-        if (newX < 0 || newX > panelWidth - 20) {
-            dx = -dx;
-            newX = getX() + dx * getSpeed();
-        }
-        if (newY < 0 || newY > panelHeight - 20) {
-            dy = -dy;
-            newY = getY() + dy * getSpeed();
-        }
-
-        setX(Math.max(0, Math.min(newX, panelWidth - 20)));
-        setY(Math.max(0, Math.min(newY, panelHeight - 20)));
-    }
-
-    public void resetStarvation(){
-        starvation = 0;
-    }
-
-    public abstract void movement();
-    public abstract void eat();
-    public abstract void reproduce();
-
-    public <E extends Entity> Optional<E> findClosest(List<E> targets){
-        E closest = null;
-        double minDist = Double.MAX_VALUE;
-
-        for (E target : targets){
-            double dist = Math.hypot(getX() - target.getX(), getY() - target.getY());
-            if (dist < minDist) {
-                minDist = dist;
-                closest = target;
-        }
-    }
-    return Optional.ofNullable(closest);
-}
 
     public int getSpeed(){
         return speed;
@@ -88,18 +17,11 @@ public abstract class Creature extends Entity {
         this.speed = speed;
     }
 
-    public int getStarvation(){
-        return starvation;
+    public int getHunger(){
+        return hunger;
     }
 
-    protected <E extends Creature> Optional<E> findNearestInRange(List<E> candidates, int range) {
-        List<E> withinRange = new ArrayList<>();
-        for (E c : candidates) {
-            if (Math.hypot(getX() - c.getX(), getY() - c.getY()) <= range) {
-                withinRange.add(c);
-            }
-        }
-        return findClosest(withinRange);
+    public void setHunger(int hunger){
+        this.hunger = hunger;
     }
-    
 }
--- predatorPrey/src/Prey.java
+++ predatorPrey/src/Prey.java
@@ -1,20 +1,9 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
 public class Prey extends Creature {
-    private boolean inDanger;
-    private List<Creature> sim;
-    private List<Grass> grassList;
-    private static final int THREAT_RANGE = 120;
-    private static final double FLEE_SPEED_MULTIPLIER = 1.8;
-    private static final int EAT_RADIUS = 10;
+    Boolean inDanger;
 
-    public Prey(int speed, int hunger,  int x, int y, List<Creature> sim, List<Grass> grassList) {
-        super(speed, hunger, false, x, y);
+    public Prey(int speed, int hunger, boolean isFood, int x, int y) {
+        super(speed, hunger, isFood, x, y);
         this.inDanger = false;
-        this.sim = sim;
-        this.grassList = grassList;
     }
 
     public boolean isInDanger(){
@@ -24,108 +13,10 @@ public class Prey extends Creature {
     public void setInDanger(boolean inDanger){
         this.inDanger = inDanger;
     }
-
-    private static final int REPRODUCE_THRESHOLD = 20;
     
-    @Override
-    public void reproduce() {
-        if (sim == null){
-            throw new IllegalStateException("Cannot reproduce: no simulation list assigned");
-        }
-        if (getStarvation() <= REPRODUCE_THRESHOLD) {
-            Prey offspring = new Prey(getSpeed(), 0, getX(), getY(), sim, grassList);
-            sim.add(offspring);
-        }
-    }
-
-
-
-    private static final int GRAZE_THRESHOLD = 60;
-
-    @Override 
     public void movement() {
-        List<Predator> predatorCandidates = new ArrayList<>();
-        for (Creature c : sim) {
-            if (c instanceof Predator p) {
-                predatorCandidates.add(p);
-            }
-        }
 
-        Optional<Predator> nearestThreat = findNearestInRange(predatorCandidates, THREAT_RANGE);
-
-        if (nearestThreat.isPresent()) {
-            setInDanger(true);
-            flee(nearestThreat.get());
-            recordPosition();
-            return;
-        }
-        setInDanger(false);
-
-        if (getStarvation() < GRAZE_THRESHOLD) {
-            recordPosition();
-            return;
-        }
-
-        List<Grass> edibleGrass = new ArrayList<>();
-        for (Grass g : grassList){
-            if (g.isEdible()) {
-                edibleGrass.add(g);
-            }
-        }
-
-        Optional<Grass> target = findClosest(edibleGrass);
-        target.ifPresent(grass -> {
-            double diffX = grass.getX() - getX();
-            double diffY = grass.getY() - getY();
-            double dist = Math.hypot(diffX, diffY);
-            if (dist == 0) return;
-            int moveX = (int) Math.round((diffX / dist) * getSpeed());
-            int moveY = (int) Math.round((diffY / dist) * getSpeed());
-            setX(getX() + moveX);
-            setY(getY() + moveY);
-        });
-
-        recordPosition();
     }
 
-    private void flee(Creature threat) {
-        double diffX = getX() - threat.getX();
-        double diffY = getY() - threat.getY();
-        double distance = Math.hypot(diffX, diffY);
-        if (distance == 0) {
-            diffX = 1;
-            distance = 1;
-        }
 
-        double boostedSpeed = getSpeed() * FLEE_SPEED_MULTIPLIER;
-        int moveX = (int) Math.round((diffX / distance) * boostedSpeed);
-        int moveY = (int) Math.round((diffY / distance) * boostedSpeed);
-
-        setX(getX() + moveX);
-        setY(getY() + moveY);
-    }
-
-    @Override
-    protected int getStarvationRate(){
-        return 1; //Prey conserve energy more effectively
-    }
-
-    @Override
-    public void eat(){
-        List<Grass> edibleGrass = new ArrayList<>();
-        for (Grass g : grassList){
-            if (g.isEdible()) {
-                edibleGrass.add(g);
-            }
-        }
-
-        Optional<Grass> closest = findClosest(edibleGrass);
-        closest.ifPresent(grass -> {
-            double dist = Math.hypot(getX() - grass.getX(), getY() - grass.getY());
-            if (dist <= EAT_RADIUS){
-                grass.setEaten(true);
-                resetStarvation();
-            }
-        });
-    }
 }
