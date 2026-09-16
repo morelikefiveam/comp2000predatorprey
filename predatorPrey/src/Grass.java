385cacbed3c7cae87a3f7655ee34a5a6c9d61bda
Oscar <oscarehalliday@gmail.com>
2026-09-16 10:33:43 +1000

    Added tickGrowth method to Grass.java

--- predatorPrey/src/Grass.java
+++ predatorPrey/src/Grass.java
@@ -1,10 +1,12 @@
 public class Grass extends Entity{
     private int growthTimer;
+    private final int maxGrowthTimer;
     private boolean isEaten;
 
     public Grass (int x, int y, int growthTimer){
         super(x, y, true);
         this.growthTimer = growthTimer;
+        this.maxGrowthTimer = growthTimer;
         this.isEaten = false;
     }
 
@@ -26,5 +28,18 @@ public class Grass extends Entity{
 
     public void setEaten(boolean isEaten){
         this.isEaten = isEaten;
+        if (isEaten) {
+            this.growthTimer = maxGrowthTimer;
+        }
+    }
+
+    public void tickGrowth(){
+        if (isEaten) {
+            growthTimer--;
+            if (growthTimer <= 0) {
+                isEaten = false;
+                growthTimer = maxGrowthTimer;
+            }
+        }
     }
 }
